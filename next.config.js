73f19568f0627db255be4387c99ce4efbb4440dd
Asif Rahman <findasifrahman@gmail.com>
2025-07-01 20:14:14 +0800

    updated https

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'gzeasylink.com',
+          },
+        ],
+        destination: 'https://www.gzeasylink.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
